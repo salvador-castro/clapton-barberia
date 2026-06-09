058391a8a2f375d8e6de15c1a4a4d09785f07206
Salvador Castro <salvacastro06@gmail.com>
2026-06-09 09:53:06 -0300

    fix url maps

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -144,7 +144,7 @@ export default function Info() {
           <div className="rounded-2xl overflow-hidden border border-gold/10 shadow-[0_8px_40px_rgba(0,0,0,0.5)] h-full min-h-125">
             <iframe
               title="Ubicación Clapton Barbershop"
-              src="https://maps.app.goo.gl/J2VYmnuq55B5fF2L6?g_st=ic"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3368.5611025515727!2d-63.2462281!3d-32.404125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95cc42d9240afb03%3A0x8d39feaa3e6a94b!2sClapton%20BarberShop!5e0!3m2!1ses-419!2sar!4v1781009460933!5m2!1ses-419!2sar"
               width="100%"
               height="100%"
               style={{ border: 0, filter: 'invert(90%) hue-rotate(180deg) brightness(0.9) contrast(1.1)' }}
