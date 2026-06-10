c78cd458b85004e5a7f3af09f464d4687055e059
Salva Castro <salvacastro06@gmail.com>
2026-06-10 17:02:35 -0300

    fixing css

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -65,7 +65,7 @@ export default function Services() {
             <div
               key={s.id}
               id={`service-${s.id}`}
-              className="glass-card service-card rounded-2xl p-10 border border-gold/10 cursor-default text-center flex flex-col items-center w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)]"
+              className="glass-card service-card rounded-2xl p-10 border border-gold/10 cursor-default text-center flex flex-col items-center w-[calc(50%-12px)] last:w-full lg:w-[calc(33.333%-16px)] lg:last:w-[calc(33.333%-16px)]"
             >
               <span className="text-5xl mb-5 block">{s.icon}</span>
               <h3
