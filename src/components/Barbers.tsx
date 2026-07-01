f4dc37dc6de672ee42b80cfe6133e033f2ee62a4
Salva Castro <salvacastro06@gmail.com>
2026-07-01 19:24:33 -0300

    fix wpp name emiliano

--- src/components/Barbers.tsx
+++ src/components/Barbers.tsx
@@ -51,7 +51,7 @@ const barbers = [
     photo: '/equipo/emilianoOficialdegui.jpeg',
     description: 'Emiliano es barbero desde 2018, aporta experiencia, profesionalismo y pasión por la barbería, siempre enfocado en la calidad de cada corte. Su llegada nos da un aire renovado a nuestro equipo.',
     schedule: 'Lun–Vie: 9 a 20 h · Sáb: 10 a 16 h',
-    wa: 'Emiliano+Oficialdegui',
+    wa: 'Emiliano',
   },
 ]
 
